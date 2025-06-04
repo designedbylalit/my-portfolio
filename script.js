0a85ce28a4885eee37e8821bc530dbb34d3ebd75
designedbylalit <lalitmanda90@gmail.com>
2025-06-04 21:37:56 +0530

    new portfolio

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const words = ["Web Developer", "Web Designer", "Frontend Developer"];
-  let wordIndex = 0;
-  let charIndex = 0;
-  let currentWord = '';
-  const typingSpeed = 100;
-  const erasingSpeed = 50;
-  const newWordDelay = 2000;
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-  function type() {
-      if (charIndex < words[wordIndex].length) {
-          currentWord += words[wordIndex].charAt(charIndex);
-          document.querySelector('.typing-animation').textContent = currentWord;
-          charIndex++;
-          setTimeout(type, typingSpeed);
-      } else {
-          setTimeout(erase, newWordDelay);
-      }
-  }
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
-  function erase() {
-      if (charIndex > 0) {
-          currentWord = currentWord.slice(0, -1);
-          document.querySelector('.typing-animation').textContent = currentWord;
-          charIndex--;
-          setTimeout(erase, erasingSpeed);
-      } else {
-          wordIndex = (wordIndex + 1) % words.length;
-          setTimeout(type, typingSpeed + 1100);
-      }
-  }
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-  type();
-});
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
 
+    // sticky navbar
+    let header = document.querySelector('header');
 
-// Animate progress bars
-const progressBars = document.querySelectorAll('.progress-done');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-progressBars.forEach(bar => {
-    setTimeout(() => {
-        bar.style.width = bar.getAttribute('data-done') + '%';
-        bar.style.opacity = 1;
-    }, 500);
-});
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
-// Animate circular skills
-const circles = document.querySelectorAll('.circle');
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
 
-circles.forEach(circle => {
-    let percent = circle.getAttribute('data-percent');
-    circle.style.setProperty('--percent', percent);
-});
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
