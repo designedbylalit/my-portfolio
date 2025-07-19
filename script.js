0274221713869f2f4fa9ac379ff0bccde41d0618
designedbylalit <lalitmanda90@gmail.com>
2025-07-19 13:27:25 +0530

    newww total

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-       let offset = sec.offsetTop - window.innerHeight / 1.5;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
